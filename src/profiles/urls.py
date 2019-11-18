5acc02eb4826cefa428f470c276f4f2c0e219256
rooneyrulz <roony0782045336@gmail.com>
2019-11-18 18:17:00 +0530

    basic profile view setup

--- src/profiles/urls.py
+++ src/profiles/urls.py
@@ -1,8 +1,32 @@
 from django.urls import path
-from .views import ProfileListView
+from .views import (
+    ProfileListView,
+    ProfileDetailView,
+    ProfileUpdateView,
+    ProfileDeleteView
+)
 
 app_name = 'profiles'
 urlpatterns = [
-    path('', ProfileListView.as_view(), name='profiles-list')
+    path(
+        '',
+        ProfileListView.as_view(),
+        name='profiles-list'
+    ),
+    path(
+        '<int:id>/detail/',
+        ProfileDetailView.as_view(),
+        name='profiles-detail'
+    ),
+    path(
+        '<int:id>/delete/',
+        ProfileDeleteView.as_view(),
+        name='profiles-delete'
+    ),
+    path(
+        '<int:id>/update/',
+        ProfileUpdateView.as_view(),
+        name='profiles-update'
+    ),
 ]
 
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views.generic import View, ListView
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View, ListView, DetailView, UpdateView, DeleteView
 
 from .models import Profile
 
@@ -14,4 +14,44 @@ class ProfileListView(ListView):
     context = super(ProfileListView, self).get_context_data(*args, **kwargs)
     context['title'] = 'Profiles'
     return context
+
+
+# PROFILE DETAIL VIEW
+class ProfileDetailView(DetailView):
+  queryset = Profile.objects.all()
+  context_object_name = 'profile'
+
+  def get_object(self, *args, **kwargs):
+    return get_object_or_404(
+      Profile,
+      pk=self.kwargs.get('id')
+    )
+
+
+# PROFILE UPDATE VIEW
+class ProfileUpdateView(UpdateView):
+  queryset = Profile.objects.all()
+  context_object_name = 'profile'
+  template_name = 'profiles/profile_update.html'
+  model = Profile
+  fields = '__all__'
+
+  def get_object(self, *args, **kwargs):
+    return get_object_or_404(
+      Profile,
+      pk=self.kwargs.get('id')
+    )
+
+
+# PROFILE DELETE VIEW
+class ProfileDeleteView(DeleteView):
+  queryset = Profile.objects.all()
+  context_object_name = 'profile'
+  template_name = 'profiles/profile_delete.html'
+
+  def get_object(self, *args, **kwargs):
+    return get_object_or_404(
+      Profile,
+      pk=self.kwargs.get('id')
+    )
 
