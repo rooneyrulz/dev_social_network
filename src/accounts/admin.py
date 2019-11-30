6fce41e454f5eda96ae68065025cba1252c12dbf
ruzny <ruzny@ruzny.com>
2019-11-30 21:25:04 +0530

    creating a custom user model

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
+from .models import User
+
+# admin.site.register(User)
 admin.site.unregister(Group)
 
 admin.site.site_header = 'Developer Social Network Administration'
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -2,5 +2,80 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class UserManager(BaseUserManager):
+	def create_user(self, username, email, first_name=None, last_name=None, password=None):
+		if not username:
+			raise ValueError('User name is required!')
+		if not email:
+			raise ValueError('Email must be provided!')
+		if not password:
+			raise ValueError('Password can not be empty!')
+
+		user = self.model(
+			username=username,
+			email=email,
+			first_name=first_name,
+			last_name=last_name
+		)
+		user.set_password(password)
+		user.active = True
+		return user
+
+	def create_superuser(self, username, email, first_name=None, last_name=None, password=None):
+		user = self.create_user(
+			username,
+			email,
+			first_name=first_name,
+			last_name=last_name,
+			password=password
+		)
+		user.active = True
+		user.staff = True
+		user.admin = True
+		return user
+
+	def create_staffuser(self, username, email, first_name=None, last_name=None, password=None):
+		user = self.create_user(
+			username,
+			email,
+			first_name=first_name,
+			last_name=last_name,
+			password=password
+		)
+		user.active = True
+		user.staff = True
+		return user
+
+
 class User(AbstractBaseUser):
-	pass
+	username = models.CharField(max_length=120, unique=True, null=True)
+	email = models.EmailField(max_length=255, unique=True, null=True)
+	first_name = models.CharField(max_length=100, blank=True, null=True)
+	last_name = models.CharField(max_length=300, blank=True, null=True)
+	admin = models.BooleanField(default=False)
+	staff = models.BooleanField(default=False)
+	active = models.BooleanField(default=True)
+
+	USERNAME_FIELD = 'username'
+	REQUIRED_FIELDS = ['email']
+
+	objects = UserManager()
+
+	def __str__(self, *args, **kwargs):
+		return self.username
+
+	@property
+	def get_fullname(self, *args, **kwargs):
+		return f'{self.first_name} {self.last_name}'
+
+	@property
+	def is_admin(self, *args, **kwargs):
+		return self.admin
+
+	@property
+	def is_staff(self, *args, **kwargs):
+		return self.staff
+
+	@property
+	def is_active(self, *args, **kwargs):
+		return self.active
